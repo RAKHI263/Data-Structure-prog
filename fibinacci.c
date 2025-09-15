e035b5d3721c99fd01b9139095748d321ccf0d5a
RAKHI263 <rakhimalviya2006@gmail.com>
2025-09-15 11:32:55 +0530

    Add files via upload

---
+++ fibinacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,a,b,c;
+	printf("entre the number");
+	scanf("%d",&n);
+	printf("fibonacci series:");
+	for(i=1;i<=n;i++)
+	{
+		printf("%d",a);
+		c=a+b;
+		a=b;
+		b=c;
+	}
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
